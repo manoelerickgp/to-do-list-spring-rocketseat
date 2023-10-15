42c380b12eaff29449ca6abcd689f35262c8900d
manoelerickgp <manoel.computacao@gmail.com>
2023-10-14 22:42:48 -0300

    Adding user repository

---
+++ backend/src/main/java/br/com/manoelerick/todolist/user/IUserRepository.java
@@ -0,0 +1,10 @@
+package br.com.manoelerick.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+}
