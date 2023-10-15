80a1b98cd386e4e3305666233c31120460315380
manoelerickgp <manoel.computacao@gmail.com>
2023-10-14 23:53:06 -0300

    feat: modifying save method

--- backend/src/main/java/br/com/manoelerick/todolist/user/UserController.java
+++ backend/src/main/java/br/com/manoelerick/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.manoelerick.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,12 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping(value = "/save")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUserName(userModel.getUserName());
         if (user != null) {
-            System.out.println("User already exist.");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exist.");
         }
-        return this.userRepository.save(userModel);
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
