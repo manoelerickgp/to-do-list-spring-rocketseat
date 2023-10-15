42bdc3ce94dacc1ea82920246fc4ce82984fa381
manoelerickgp <manoel.computacao@gmail.com>
2023-10-14 22:40:22 -0300

    Implementing ORM

--- backend/src/main/java/br/com/manoelerick/todolist/user/UserModel.java
+++ backend/src/main/java/br/com/manoelerick/todolist/user/UserModel.java
@@ -1,12 +1,33 @@
 package br.com.manoelerick.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Data
+@Entity
 public class UserModel {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String userName;
+
+    @Column(nullable = false)
     private String password;
+
+    @CreationTimestamp
+    private Instant createdAt;
 
 }
