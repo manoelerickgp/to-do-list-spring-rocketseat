073d4b66c616afe17893f17e20a2bce407a471c5
manoelerickgp <manoel.computacao@gmail.com>
2023-10-21 01:42:36 -0300

    Adding hash password in the user model

--- backend/src/main/java/br/com/manoelerick/todolist/user/UserController.java
+++ backend/src/main/java/br/com/manoelerick/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.manoelerick.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,9 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exist.");
         }
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashred);
+
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
